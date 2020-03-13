c595e1a8476c42b66c5474b11452a293dfada512
IvanXBet <ivan24072001@gamil.com>
2020-03-13 22:10:46 +0300

    first commit

---
+++ Vedomost/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Vedomost
+{
+    partial class Vedomost
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxNumStud = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabPage3);
+            this.tabControl.Controls.Add(this.tabPage4);
+            this.tabControl.Location = new System.Drawing.Point(1, 2);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(964, 473);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBoxNumStud);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(956, 447);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Успеваемость студентов";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(35, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество студентов";
+            // 
+            // comboBoxNumStud
+            // 
+            this.comboBoxNumStud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxNumStud.FormattingEnabled = true;
+            this.comboBoxNumStud.Location = new System.Drawing.Point(189, 408);
+            this.comboBoxNumStud.Name = "comboBoxNumStud";
+            this.comboBoxNumStud.Size = new System.Drawing.Size(44, 21);
+            this.comboBoxNumStud.TabIndex = 1;
+            this.comboBoxNumStud.SelectionChangeCommitted += new System.EventHandler(this.comboBoxNumStud_SelectionChangeCommitted);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(939, 373);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(956, 447);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Успеваемость по предметам";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(956, 447);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Стипендия";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(956, 447);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Успеваемость диаграмма";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(8, 6);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(939, 373);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(748, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Vedomost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 477);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Vedomost";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Vedomost_Load);
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxNumStud;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Vedomost/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vedomost
+{
+    public partial class Vedomost : Form
+    {
+        public Vedomost()
+        {
+            InitializeComponent();
+
+            //Создание колонок dataGridView1
+
+            DataGridViewTextBoxColumn Name = new DataGridViewTextBoxColumn();
+            Name.Name = "Name";
+            Name.HeaderText = "Имя Фамилия";
+            Name.Width = 150;
+            dataGridView1.Columns.Add(Name);
+
+            DataGridViewTextBoxColumn maths = new DataGridViewTextBoxColumn();
+            maths.Name = "maths";
+            maths.HeaderText = "Математика";
+            maths.Width = 100;
+            dataGridView1.Columns.Add(maths);
+
+            DataGridViewTextBoxColumn physics = new DataGridViewTextBoxColumn();
+            physics.Name = "maths";
+            physics.HeaderText = "Физика";
+            physics.Width = 100;
+            dataGridView1.Columns.Add(physics);
+
+            DataGridViewTextBoxColumn pe = new DataGridViewTextBoxColumn();
+            pe.Name = "pe";
+            pe.HeaderText = "Физкультура";
+            pe.Width = 100;
+            dataGridView1.Columns.Add(pe);
+
+            /////////////////////////////
+
+            //Создание колонок dataGridView2
+
+            DataGridViewTextBoxColumn NameDG2 = new DataGridViewTextBoxColumn();
+            NameDG2.Name = "NameDG2";
+            NameDG2.HeaderText = "Имя Фамилия";
+            NameDG2.Width = 150;
+            dataGridView2.Columns.Add(NameDG2);
+
+            DataGridViewTextBoxColumn NA = new DataGridViewTextBoxColumn();
+            NA.Name = " NA";
+            NA.HeaderText = "Н/А";
+            NA.Width = 50;
+            dataGridView2.Columns.Add(NA);
+
+            DataGridViewTextBoxColumn two = new DataGridViewTextBoxColumn();
+            two.Name = "two";
+            two.HeaderText = " 2";
+            two.Width = 50;
+            dataGridView2.Columns.Add(two);
+
+            DataGridViewTextBoxColumn three = new DataGridViewTextBoxColumn();
+            three.Name = "three";
+            three.HeaderText = " 3";
+            three.Width = 50;
+            dataGridView2.Columns.Add(three);
+
+            DataGridViewTextBoxColumn four = new DataGridViewTextBoxColumn();
+            four.Name = "four";
+            four.HeaderText = " 4";
+            four.Width = 50;
+            dataGridView2.Columns.Add(four);
+
+            DataGridViewTextBoxColumn five = new DataGridViewTextBoxColumn();
+            five.Name = "five";
+            five.HeaderText = " 5";
+            five.Width = 50;
+            dataGridView2.Columns.Add(five);
+
+            DataGridViewTextBoxColumn aver = new DataGridViewTextBoxColumn();
+            aver.Name = "aver";
+            aver.HeaderText = " Сред. знач.";
+            aver.Width = 150;
+            dataGridView2.Columns.Add(aver);
+
+
+
+        }
+
+        private void Vedomost_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 21; i++) {
+                comboBoxNumStud.Items.Add(i);
+            }
+        }
+
+        private void comboBoxNumStud_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            //Создание строк
+
+            int numLins = Convert.ToInt32(comboBoxNumStud.Text) + 1;
+
+            //Создание строк Если ещё пустая
+            if (dataGridView1.RowCount == 0)
+            {
+
+                for (int i = 1; i < numLins; i++)
+                {
+                    if (dataGridView1.RowCount > 19)
+                        break;
+                    else
+                        dataGridView1.Rows.Add();
+                }
+            }
+            //Создание строк Если уже есть и нужно добавить
+            else
+            {
+
+                for (int i = 0; i <= numLins - dataGridView1.RowCount; i++)
+                {
+                    if (dataGridView1.RowCount > 19)
+                        break;
+                    else
+                        dataGridView1.Rows.Add();
+                }
+            }
+
+
+
+
+        }
+
+
+        //Создаём строки в DG2 по DG1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numLins = Convert.ToInt32(comboBoxNumStud.Text) + 1;
+
+            if (dataGridView2.RowCount == 0)
+
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    dataGridView2.Rows.Add();
+                }
+
+            else
+            {
+                
+                for (int i = 0; i < numLins - dataGridView1.RowCount ; i++)
+                {
+                    dataGridView2.Rows.Add();
+                }
+            }
+
+    
+        }
+    }
+}
